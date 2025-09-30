036f8f463a567b0088ad93dc2a1ff6e6e5d578ff
anway001 <anwaykharsamble@gmail.com>
2025-09-30 19:53:44 +0530

    some changes1

--- Frontend/src/Components/AdminProduct.jsx
+++ Frontend/src/Components/AdminProduct.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './AdminProduct.css';
+import './Adminproduct.css';
+
+// 
 
 function AdminProduct() {
     const [products, setProduct] = useState([]);
